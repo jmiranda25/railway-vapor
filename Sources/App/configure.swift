a6c970e08ec7651e6a2a9fc635cd81ae132fcd4d
Dennis Dang <dangggdennis@gmail.com>
2025-06-08 02:29:57 -0400

    remove postgres tls

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -15,7 +15,6 @@ public func configure(_ app: Application) async throws {
         username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
         password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
         database: Environment.get("DATABASE_NAME") ?? "vapor_database",
-        tls: .prefer(try .init(configuration: .clientDefault)))
     ), as: .psql)
 
     app.migrations.add(CreateTodo())
