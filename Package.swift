5614a6be86b10a791a52f731b83a683aa5c9cc1b
Dennis Dang <dangggdennis@gmail.com>
2025-06-08 02:14:12 -0400

    switch to postgres

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "4.115.0"),
         // 🗄 An ORM for SQL and NoSQL databases.
         .package(url: "https://github.com/vapor/fluent.git", from: "4.9.0"),
-        // 🪶 Fluent driver for SQLite.
-        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.6.0"),
+        // 🐘 Fluent driver for Postgres.
+        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.8.0"),
         // 🍃 An expressive, performant, and extensible templating language built for Swift.
         .package(url: "https://github.com/vapor/leaf.git", from: "4.3.0"),
         // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
@@ -23,7 +23,7 @@ let package = Package(
             name: "App",
             dependencies: [
                 .product(name: "Fluent", package: "fluent"),
-                .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
+                .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
                 .product(name: "Leaf", package: "leaf"),
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "NIOCore", package: "swift-nio"),
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,6 +1,6 @@
 import NIOSSL
 import Fluent
-import FluentSQLiteDriver
+import FluentPostgresDriver
 import Leaf
 import Vapor
 
@@ -9,10 +9,14 @@ public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    // Use absolute path for production, relative for development.
-    // /app is the configured volume path in the Railway template.
-    let dbPath = app.environment == .production ? "/app/db.sqlite" : "db.sqlite"
-    app.databases.use(DatabaseConfigurationFactory.sqlite(.file(dbPath)), as: .sqlite)
+    app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
+        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
+        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+        database: Environment.get("DATABASE_NAME") ?? "vapor_database",
+        tls: .prefer(try .init(configuration: .clientDefault)))
+    ), as: .psql)
 
     app.migrations.add(CreateTodo())
 
